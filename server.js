41416c0c3b36ad48220c4ef20f6f17989701a1ef
miraclebanks <miraclebnks@gmail.com>
2020-06-14 11:31:29 -0700

    connecting database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-amorphous-87069",
-    user: "miracle",
-    password: "",
-    database: "faceRecog",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
